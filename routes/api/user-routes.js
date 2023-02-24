7e22be228c34ed62a681125c0eb793dabeea7999
samuel ezadmanesh <samezad27@gmail.com>
2023-02-23 18:51:19 -0800

    need to fix my database

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,40 +1,36 @@
-
-const router = require('express').Router();
-const {User} = require("../../models")
+const router = require("express").Router();
+const { User } = require("../../models");
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
-router.get('/', (req,res)=> {
-
-})
+router.get("/", (req, res) => {});
 
 //TODO - ROUTE THAT CREATES A NEW USER
-router.post('/', (req,res)=> {
-
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+  })
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
-router.get('/:userId', (req,res) => {
-
-})
+router.get("/:userId", (req, res) => {});
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
-router.put('/:userId', (req,res)=> {
-
-})
+router.put("/:userId", (req, res) => {});
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
-router.delete('/:userId', (req,res)=> {
-
-});
+router.delete("/:userId", (req, res) => {});
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
-router.put('/:userId/friends/:friendId', (req,res)=> {
-
-})
+router.put("/:userId/friends/:friendId", (req, res) => {});
 
 //TODO - ROUTE THAT DELETES A FRIEND FROM A USER'S FRIENDS, DONT DELETE THE FRIEND AS A USER THOUGH!
-router.delete('/:userId/friends/:friendId', (req,res)=> {
-  
-});
+router.delete("/:userId/friends/:friendId", (req, res) => {});
 
 module.exports = router;
